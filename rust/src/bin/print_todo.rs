e6490a878e0652ce59c5e6a59ed5bbee49ffb7dc
Arne Beer <contact@arne.beer>
2022-01-16 17:36:27 +0100

    Configurable path and progress meter

--- rust/src/bin/print_todo.rs
+++ rust/src/bin/print_todo.rs
@@ -1,34 +1,91 @@
-use anyhow::Result;
+use std::{fs::read_to_string, path::PathBuf, str::Lines};
 
-use script_utils::{path::read_file, path_exists};
+use anyhow::Result;
+use clap::Parser;
+use script_utils::Context;
+
+#[derive(Parser, Debug)]
+pub struct CliArguments {
+    /// The path to the todo markdown file.
+    pub path: PathBuf,
+}
 
 /// Simply read a file and print a few lines of output
 fn main() -> Result<()> {
-    let path = "~/Syncthing/Transfer/todo";
+    // Parse commandline options.
+    let args = CliArguments::parse();
 
-    if !path_exists(path) {
+    if !args.path.exists() {
         println!("Nothing to do :)");
         return Ok(());
     }
 
-    let content = read_file(path)?;
-    let concat = content
-        .lines()
-        .filter(|line| line.starts_with("#"))
-        .map(|line| {
-            let line = line.clone().strip_prefix("#");
-            let line = line.unwrap().trim();
-            line.to_string()
-        })
-        .collect::<Vec<String>>()
-        .join(", ");
+    let mut output = String::new();
+    let content = read_to_string(args.path).context("Failed to read file")?;
+    let mut lines = content.lines();
 
-    if concat.trim().is_empty() {
+    let mut next_todo = lines
+        .find(|line| line.starts_with("#"))
+        .map(|line| line.to_string());
+
+    loop {
+        if let Some(headline) = next_todo {
+            next_todo = handle_todo_items(&headline, &mut lines, &mut output);
+        } else {
+            break;
+        }
+    }
+
+    if output.trim().is_empty() {
         println!("Nothing to do :) |");
         return Ok(());
     }
 
-    println!("todos: {concat} |");
+    println!("todos: {output} |");
 
     Ok(())
+}
+
+/// Go through all lines of a todo text and extract information from it.
+/// For example, the amount items that were completed.
+///
+/// Retuns the next todo headline, we hit one.
+fn handle_todo_items(headline: &str, lines: &mut Lines, output: &mut String) -> Option<String> {
+    // First things first, append the name of the todo.
+    if let Some(headline) = headline.strip_prefix("#") {
+        output.push_str(headline.trim());
+    }
+
+    let mut items = 0;
+    let mut completed_items = 0;
+    for line in lines {
+        // We found the next todo. Abort.
+        if line.starts_with("#") {
+            // Add the current item counter and a comma for todo separation.
+            if items > 0 {
+                output.push_str(&format!(" ({completed_items}/{items})"))
+            }
+            output.push_str(", ");
+            return Some(line.to_string());
+        }
+
+        // We found an unfinished item
+        if line.trim().starts_with("- [ ]") || line.trim().starts_with("- []") {
+            items += 1;
+            continue;
+        }
+
+        // We found a finished item
+        if line.trim().starts_with("- [x]") {
+            items += 1;
+            completed_items += 1;
+        }
+    }
+
+    // Add the current item counter.
+    if items > 0 {
+        output.push_str(&format!(" ({completed_items}/{items})"))
+    }
+
+    None
 }
