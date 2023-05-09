a617525ef69783ead3c63c3ff700f9d8fef1eac3
Arne Beer <contact@arne.beer>
2023-05-09 23:33:11 +0200

    add: Install global script

---
+++ install_global.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Some best practice settings for bash scripts
+# set -e: automatically exits on any failing command
+# set -u: exits if there are any unset variables
+# set -o pipefail: automatically exits, if any command in a pipe fails (normally only the last is counted)
+set -euo pipefail
+
+cp hooks/* .git/hooks
+
+# Get absolute path this script's directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+BIN_FOLDER="/usr/local/bin"
+
+echo "Deploying shell scripts"
+for file in $DIR/shell/* ; do
+    file_name=$(basename $file)
+    if [ ! -L "$HOME/.bin/${file_name}" ]; then
+        sudo ln -f -s $file $BIN_FOLDER/$file_name
+    fi
+done
+
+# Rust scripts
+echo "Installing rust scripts"
+cargo build --release
+cd target/release/
+sudo fd . ./ --type x --maxdepth 1 --exec cp {} /usr/local/bin/
