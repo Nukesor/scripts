44f08586a77bc11d92df0bbf0d2f7d7019ad89c5
Arne Beer <contact@arne.beer>
2024-01-04 12:55:22 +0100

    fix: netinfo for perfect connection

--- src/bin/netinfo.rs
+++ src/bin/netinfo.rs
@@ -150,7 +150,7 @@ pub fn wifi_strength(interface: &str) -> &'static str {
     };
 
     match level {
-        10..=30 => "▇",
+        01..=30 => "▇",
         51..=67 => "▅",
         68..=70 => "▃",
         71..=80 => "▁",
