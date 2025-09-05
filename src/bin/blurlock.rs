bb5c0818e571dbc2b6b60984e508bba29a02e63c
Arne Beer <contact@arne.beer>
2025-09-06 00:46:44 +0200

    feat: Blurlock for hyprlock

--- src/bin/blurlock.rs
+++ src/bin/blurlock.rs
@@ -4,23 +4,37 @@
 //! 2. Run a custom point filter on the image data.
 //! 3. Save it.
 use std::{
-    fs::remove_file,
+    fs::{File, remove_file},
     io::Write,
+    path::Path,
     process::{Command, Stdio},
     time::Instant,
 };
 
 use anyhow::{Context, Result};
 use clap::{ArgAction, Parser};
-use image::{DynamicImage, ImageBuffer, ImageReader, Pixel, Rgb, RgbImage};
+use dirs::runtime_dir;
+use image::{
+    DynamicImage,
+    ImageBuffer,
+    ImageReader,
+    Pixel,
+    Rgb,
+    RgbImage,
+    codecs::webp::WebPEncoder,
+};
 use log::debug;
 use rayon::{
     iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator},
     slice::ParallelSliceMut,
 };
-use script_utils::{bail, logging, prelude::*};
+use script_utils::{bail, logging};
 
-const SCREENSHOT_PATH: &str = "/tmp/blur-screenshot.jpg";
+#[derive(clap::ValueEnum, Clone, Copy, Debug)]
+enum Mode {
+    I3Lock,
+    Hyprlock,
+}
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -31,7 +45,12 @@ const SCREENSHOT_PATH: &str = "/tmp/blur-screenshot.jpg";
 struct CliArguments {
     /// The scale we should blur to.
     /// I.e. `5` would result in a relative 20% downscale.
+    #[clap(default_value = "5")]
     pub scale: usize,
+
+    /// i3-lock or hyprlock
+    #[clap(default_value = "hyprlock")]
+    pub mode: Mode,
 
     /// Verbose mode (-v, -vv, -vvv)
     #[clap(short, long, action = ArgAction::Count)]
@@ -43,69 +62,129 @@ fn main() -> Result<()> {
     let args = CliArguments::parse();
     logging::init_logger(args.verbose);
 
-    // Make screenshot and init the image.
-    get_screenshot()?;
-    let mut image = load_image()?;
+    let runtime_dir = runtime_dir().context("Expected to find runtime dir.")?;
 
-    // Blur the image.
+    // Make screenshot and init the image.
+    let screenshot_path = runtime_dir.join("screenshot.jpg");
+    get_screenshot(&screenshot_path, &args.mode)?;
+    let mut image = load_image(&screenshot_path)?;
+
+    // Blur the image and write it the file.
     image = blur_image(args.scale, image)?;
 
-    let start = Instant::now();
-    // Spawn i3lock directly from in here.
-    let (width, height) = image.dimensions();
-    #[allow(clippy::zombie_processes)]
-    let mut child = Command::new("i3lock")
-        .arg("--show-failed-attempts")
-        .arg("--raw")
-        .arg(format!("{width}x{height}:rgb"))
-        .arg("--image")
-        .arg("/dev/stdin")
-        .env("DISPLAY", ":0")
-        .stdin(Stdio::piped())
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .spawn()
-        .expect("failed to execute child");
+    match args.mode {
+        Mode::Hyprlock => {
+            let start = Instant::now();
+            let path = runtime_dir.join("wallpaper.webp");
+            if path.exists() {
+                remove_file(&path).context("Failed to remove old wallpaper")?;
+            }
+            let mut file = File::create(&path).context("Failed to open wallpaper file")?;
 
-    // Send i3lock the image in raw form via stdin.
-    let mut stdin = child.stdin.take().context("Failed to get stdin handle.")?;
-    stdin
-        .write(&image.into_raw())
-        .context("Failed to send image to i3lock stdin")?;
+            let encoder = WebPEncoder::new_lossless(&mut file);
+            image.write_with_encoder(encoder)?;
 
-    debug!(
-        "I3lock spawn + image copy: {}ms",
-        start.elapsed().as_millis()
-    );
+            debug!(
+                "Hyprlock writing file took {}ms",
+                start.elapsed().as_millis()
+            );
 
-    // i3lock instantly forks away on it's own, but we should still wait for it to do that.
-    child.wait().context("Failed to wait for i3lock.")?;
+            Command::new("hyprlock")
+                .output()
+                .expect("failed to execute hyprlock");
+        }
+        Mode::I3Lock => {
+            let start = Instant::now();
+            // Spawn i3lock directly from in here.
+            let (width, height) = image.dimensions();
+
+            #[allow(clippy::zombie_processes)]
+            let mut child = Command::new("hyprlock")
+                .arg("--show-failed-attempts")
+                .arg("--raw")
+                .arg(format!("{width}x{height}:rgb"))
+                .arg("--image")
+                .arg("/dev/stdin")
+                .env("DISPLAY", ":0")
+                .stdin(Stdio::piped())
+                .stderr(Stdio::inherit())
+                .stdout(Stdio::inherit())
+                .spawn()
+                .expect("failed to execute child");
+
+            // Send i3lock the image in raw form via stdin.
+            let mut stdin = child.stdin.take().context("Failed to get stdin handle.")?;
+            stdin
+                .write(&image.into_raw())
+                .context("Failed to send image to i3lock stdin")?;
+
+            debug!(
+                "I3lock spawn + image copy: {}ms",
+                start.elapsed().as_millis()
+            );
+
+            // i3lock instantly forks away on it's own, but we should still wait for it to do that.
+            child.wait().context("Failed to wait for i3lock.")?;
+        }
+    }
 
     Ok(())
 }
 
 /// Make a screenshot via scrot and capture the image (png) bytes.
-fn get_screenshot() -> Result<()> {
+fn get_screenshot(path: &Path, mode: &Mode) -> Result<()> {
     let start = Instant::now();
-    Cmd::new(format!(
-        "scrot --overwrite --delay 0 --quality 95 --silent {SCREENSHOT_PATH}"
-    ))
-    .run_success()?;
-    debug!("scrot execution time: {}ms", start.elapsed().as_millis());
+    match mode {
+        Mode::I3Lock => {
+            let output = Command::new("scrot")
+                .args(["--overwrite", "--delay", "0", "--quality", "40", "--output"])
+                .arg(path.to_string_lossy().to_string())
+                .output()
+                .expect("failed to execute scrot");
+
+            if !output.status.success() {
+                bail!(
+                    "Failed to run scrot command!\nstdout: {}\nstderr: {}",
+                    String::from_utf8_lossy(&output.stdout),
+                    String::from_utf8_lossy(&output.stderr),
+                )
+            }
+        }
+        Mode::Hyprlock => {
+            let output = Command::new("grim")
+                .args(["-t", "jpeg", "-q", "40"])
+                .arg(path.to_string_lossy().to_string())
+                .output()
+                .expect("failed to execute grim");
+
+            if !output.status.success() {
+                bail!(
+                    "Failed to run scrot command!\nstdout: {}\nstderr: {}",
+                    String::from_utf8_lossy(&output.stdout),
+                    String::from_utf8_lossy(&output.stderr),
+                )
+            }
+        }
+    }
+
+    debug!(
+        "screenshot execution time: {}ms",
+        start.elapsed().as_millis()
+    );
 
     Ok(())
 }
 
 /// Initialize the image from the raw bytes.
-fn load_image() -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>> {
+fn load_image(path: &Path) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>> {
     let start = Instant::now();
 
-    let image = ImageReader::open(SCREENSHOT_PATH)?.decode()?;
+    let image = ImageReader::open(path)?.decode()?;
     let image = match image {
         DynamicImage::ImageRgb8(image) => image,
         _ => bail!("Expected Rgb8 format from scrot"),
     };
-    remove_file(SCREENSHOT_PATH).context("Failed to remove screenshot.")?;
+    //remove_file(path).context("Failed to remove screenshot.")?;
 
     debug!("Image init time: {}ms", start.elapsed().as_millis());
     Ok(image)
